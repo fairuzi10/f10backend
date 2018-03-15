5c1cbc584e9ead02e1ecd91dd790449d544d4c19
M Fairuzi Teguh <mfairuzit@gmail.com>
2018-03-16 00:18:53 +0700

    Add cors

--- f10backend/settings.py
+++ f10backend/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
     'rest_auth.registration',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
+    'corsheaders',
     'saved_messages.apps.SavedMessagesConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
